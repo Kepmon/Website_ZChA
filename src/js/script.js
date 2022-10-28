dcf45e41caf8aac6235479a60f51567b1454f36a
Monika <monika.kepasa@gmail.com>
2022-10-28 23:57:55 +0200

    applied some idea for highlighting a menu item

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,24 @@
 const body = document.querySelector('body');
+const main = document.querySelector('.main');
 const themeLink = document.querySelector('.theme');
 const languageLink = document.querySelector('.language');
 const navItemsMain = Array.from(document.querySelectorAll('.down'));
+const navItems = document.querySelector('.nav__items');
+const allNavItemsMain = Array.from(
+	document.querySelectorAll('.nav__item-main')
+);
 const ulItemsDown = Array.from(document.querySelectorAll('.nav__items-down'));
 const navItemsDown = document.querySelectorAll('.nav__item-down');
-const navItemsMobileMain = Array.from(document.querySelectorAll('.mobile-down'))
+const navItemsMobileMain = Array.from(
+	document.querySelectorAll('.mobile-down')
+);
+const allMobileItemsMain = Array.from(document.querySelectorAll('.nav__mobile-main'))
 const ulItemsMobileDown = Array.from(
 	document.querySelectorAll('.nav__mobile-items-down')
 );
 const navBurger = document.querySelector('.nav__burger');
 const navMobileItems = document.querySelector('.nav__mobile-items');
-const year = document.querySelector('.year')
+const year = document.querySelector('.year');
 
 const changeTheme = () => {
 	if (body.getAttribute('data-mode') === 'dark') {
@@ -25,9 +33,7 @@ for (let i = 0; i < navItemsMain.length; i++) {
 		ulItemsDown.forEach((item) => {
 			if (ulItemsDown.indexOf(item) !== navItemsMain.indexOf(e.target)) {
 				item.classList.remove('nav-active');
-			}
-			else
-			{
+			} else {
 				item.classList.toggle('nav-active');
 			}
 		});
@@ -36,35 +42,84 @@ for (let i = 0; i < navItemsMain.length; i++) {
 	navItemsMain[i].addEventListener('click', showItemsDown);
 }
 
-const removeItemsDown = (e) => {
-	const navItems = document.querySelector('.nav__items');
-	const allNavItemsMain = Array.from(
-		document.querySelectorAll('.nav__item-main')
-	);
+for (let i = 0; i < allNavItemsMain.length; i++) {
+	const highlightNavMain = () => {
+		switch (true) {
+			case main.classList.contains('home'):
+				allNavItemsMain[0].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('courses'):
+				allNavItemsMain[1].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('speciality'):
+				allNavItemsMain[2].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('staff'):
+				allNavItemsMain[3].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('research'):
+				allNavItemsMain[4].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('labs'):
+				allNavItemsMain[5].classList.add('menu-item-active');
+				break;
+		}
+	};
+	window.addEventListener('DOMContentLoaded', highlightNavMain);
+}
 
+const removeItemsDown = (e) => {
 	if (e.target !== navItems && allNavItemsMain.indexOf(e.target) === -1) {
 		ulItemsDown.forEach((item) => item.classList.remove('nav-active'));
 	}
 };
 
 for (let i = 0; i < navItemsMobileMain.length; i++) {
-	const showItemsMobileDown = e => {
+	const showItemsMobileDown = (e) => {
 		ulItemsMobileDown.forEach((item) => {
-			if (ulItemsMobileDown.indexOf(item) !== navItemsMobileMain.indexOf(e.target)) {
+			if (
+				ulItemsMobileDown.indexOf(item) !== navItemsMobileMain.indexOf(e.target)
+			) {
 				item.classList.remove('mobile-down-active');
-			}
-			else
-			{
+			} else {
 				item.classList.toggle('mobile-down-active');
 			}
-		})
-		console.log(e.target);
-	}
+		});
+	};
 
 	navItemsMobileMain[i].addEventListener('click', showItemsMobileDown);
 }
 
-const showMobileItems = () => navMobileItems.classList.toggle('nav-mobile-active')
+const showMobileItems = () =>
+	navMobileItems.classList.toggle('nav-mobile-active');
+
+for (let i = 0; i < allMobileItemsMain.length; i++) {
+	const highlightMobileMain = () => {
+
+		switch (true) {
+			case main.classList.contains('home'):
+				console.log('true');
+				allMobileItemsMain[0].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('courses'):
+				allMobileItemsMain[1].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('speciality'):
+				allMobileItemsMain[2].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('staff'):
+				allMobileItemsMain[3].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('research'):
+				allMobileItemsMain[4].classList.add('menu-item-active');
+				break;
+			case main.classList.contains('labs'):
+				allMobileItemsMain[5].classList.add('menu-item-active');
+				break;
+		}
+	};
+	window.addEventListener('DOMContentLoaded', highlightMobileMain);
+}
 
 const removeMobileItemsDown = () => {
 	if (navMobileItems.classList.contains('nav-mobile-active')) {
@@ -77,12 +132,12 @@ const removeMobileItemsDown = () => {
 };
 
 const displayYear = () => {
-	const getYear = new(Date)
-	year.textContent = getYear.getFullYear()
-}
+	const getYear = new Date();
+	year.textContent = getYear.getFullYear();
+};
 
 themeLink.addEventListener('click', changeTheme);
 window.addEventListener('click', removeItemsDown);
 navBurger.addEventListener('click', showMobileItems);
 navBurger.addEventListener('click', removeMobileItemsDown);
-window.addEventListener('DOMContentLoaded', displayYear)
+window.addEventListener('DOMContentLoaded', displayYear);
