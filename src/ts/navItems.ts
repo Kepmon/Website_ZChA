50c39b0eca184c22f71208cae2aeed5d476eb973
Monika <monika.kepasa@gmail.com>
2023-05-15 00:26:35 +0200

    replaced the hardcoded nav items with a loop iterating over a respective object's properties

---
+++ src/ts/navItems.ts
@@ -0,0 +1,70 @@
+interface NavItem {
+    name: string,
+    id?: string,
+    url?: string,
+    subitems?: NavSubitem[]
+}
+
+interface NavSubitem {
+    name: string,
+    url: string
+}
+
+export const navItems: NavItem[] = [
+    {
+        name: 'Strona główna',
+        url: '/'
+    },
+    {
+        name: 'Dydaktyka',
+        id: 'teaching-dropdown',
+        subitems: [
+            {
+                name: 'Studia inżynierskie I stopnia',
+                url: '/dydaktyka_I_stopien'
+            },
+            {
+                name: 'Studia inżynierskie niestacjonarne',
+                url: '/dydaktyka_niestacjonarne'
+            },
+            {
+                name: 'Studia magisterskie II stopnia',
+                url: '/dydaktyka_II_stopien'
+            }
+        ]
+    },
+    {
+        name: 'Specjalność',
+        url: '/specjalnosc',
+    },
+    {
+        name: 'Pracownicy',
+        id: 'employees-dropdown',
+        subitems: [
+            {
+                name: 'Zespół Chemii Analitycznej',
+                url: '/zespol_chemii_analitycznej'
+            },
+            {
+                name: 'Zespół metalurgii chemicznej',
+                url: '/zespool_metalurgii'
+            },
+            {
+                name: '/doktoranci',
+                url: '/dydaktyka_II_stopien'
+            },
+            {
+                name: 'Konsultacje',
+                url: '/konsultacje'
+            }
+        ]
+    },
+    {
+        name: 'Badania',
+        url: '/badania',
+    },
+    {
+        name: 'Laboratoria',
+        url: '/laboratoria',
+    },
+]
