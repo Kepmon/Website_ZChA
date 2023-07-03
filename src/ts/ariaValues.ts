10d67a668808de0fe6b7dc763c5b494e408362e8
Monika <monika.kepasa@gmail.com>
2023-07-03 02:15:38 +0200

    added some improvements to the existing code

--- src/ts/ariaValues.ts
+++ src/ts/ariaValues.ts
@@ -1,8 +1,12 @@
 type Element = HTMLDivElement | HTMLAnchorElement | HTMLButtonElement
-export const setAriaValues = (element: Element) => {
+
+export const setAriaValues = (element: Element, observer: ResizeObserver) => {
     if (element.getAttribute('aria-expanded') === 'false') {
+        observer.unobserve(element.nextElementSibling as HTMLUListElement)
+        element.nextElementSibling?.removeAttribute('aria-hidden')
         element.setAttribute('aria-expanded', 'true')
         return
     }
+    observer.observe(element.nextElementSibling as HTMLUListElement)
     element.setAttribute('aria-expanded', 'false')
 }
